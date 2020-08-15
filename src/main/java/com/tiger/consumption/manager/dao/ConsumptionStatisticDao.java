e974dd4e381a0092cc34244992196be5d9006d01
ZengHu <978689162@qq.com>
2020-08-15 21:34:05 +0800

    添加缓存

--- src/main/java/com/tiger/consumption/manager/dao/ConsumptionStatisticDao.java
+++ src/main/java/com/tiger/consumption/manager/dao/ConsumptionStatisticDao.java
@@ -6,6 +6,7 @@ import com.tiger.consumption.manager.model.MonthConsumptionStatistic;
 import com.tiger.consumption.manager.model.PayType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -20,9 +21,10 @@ import java.util.List;
 @Repository
 @Mapper
 public interface ConsumptionStatisticDao {
+
     List<ConsumptionStatistic> statisticConsumption(@Param("begin") Date begin, @Param("end") Date end, @Param(
             "consumptionType") ConsumptionType consumptionType, @Param("payType") PayType payType);
-
+    @Cacheable(cacheNames = {"monthConsumptionStatistic"}, key = "'statistic:'+#year+'-'+#month")
     List<MonthConsumptionStatistic> statisticConsumptionByMonth(@Param("year") int year, @Param("month") int month);
 
 }
